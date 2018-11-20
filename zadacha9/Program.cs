b7ee62fcf11e38b2ce7421f856a6c46123b97ef1
KhalyavinSergey <44732519+KhalyavinSergey@users.noreply.github.com>
2018-11-20 20:20:07 +0300

    Add files via upload

---
+++ zadacha9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += 3 / (9 * (Math.Pow(n, 2)) + 3 * n - 2);
+
+
+            }
+            Console.WriteLine("Sum = {0}", s);
+            Console.ReadLine();
+        }
+    }
+}
